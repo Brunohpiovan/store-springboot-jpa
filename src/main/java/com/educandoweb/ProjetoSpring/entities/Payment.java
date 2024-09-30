0941df4b5bdbcd380d7bb28bf7cace17b6230085
Bruno piovan <bpiovan90@gmail.com>
2024-09-30 17:02:13 -0300

    Métodos subtotal e total

--- src/main/java/com/educandoweb/ProjetoSpring/entities/Payment.java
+++ src/main/java/com/educandoweb/ProjetoSpring/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.educandoweb.ProjetoSpring.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -19,6 +20,7 @@ public class Payment implements Serializable {
     private Long id;
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
