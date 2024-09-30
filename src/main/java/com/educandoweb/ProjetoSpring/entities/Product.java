2ac7abc0bbd656d8a365a113f056c3213e75a2d7
Bruno piovan <bpiovan90@gmail.com>
2024-09-30 15:50:29 -0300

    Associação muitos para muitos entre Product e OrderItem

--- src/main/java/com/educandoweb/ProjetoSpring/entities/Product.java
+++ src/main/java/com/educandoweb/ProjetoSpring/entities/Product.java
@@ -31,6 +31,10 @@ public class Product implements Serializable {
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    @OneToMany(mappedBy = "id.product", fetch = FetchType.EAGER)
+    private Set<OrderItem> items = new HashSet<>();
+
+
     public Product() {
     }
 
@@ -84,6 +88,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for(OrderItem x : items){
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
